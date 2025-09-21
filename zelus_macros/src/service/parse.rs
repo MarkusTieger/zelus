87adc90576a281fd2051480aa3c22127952e655c
Markus Probst <markus.probst@posteo.de>
2025-09-21 19:52:30 +0200

    Replace copyright header with spdx-license-header
    To provide constistancy with the daemon

--- zelus_macros/src/service/parse.rs
+++ zelus_macros/src/service/parse.rs
@@ -1,17 +1,4 @@
-/*
- * Copyright (C) 2025 Markus Probst
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, version 3.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- */
+// SPDX-License-Identifier: AGPL-3.0-only
 use proc_macro2::{Group, Ident, TokenStream};
 use syn::LitStr;
 
